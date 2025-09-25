76a1a7a560fce45b9ef368bd7a980ae321fafde7
Ajay Sharma <elite.ajaysharma@gmail.com>
2025-09-25 13:57:04 +0530

    Fix loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 "use client"
 
+import { Loader } from "lucide-react";
 import { useEffect, useMemo } from "react";
 import { useRouter } from "next/navigation";
-
-import { UserButton } from "@/features/auth/components/user-button";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useCreateWorkspaceModal } from "../features/workspaces/store/use-create-workspace-modal";
@@ -11,22 +10,22 @@ import { useCreateWorkspaceModal } from "../features/workspaces/store/use-create
 export default function Home() {
   const router = useRouter();
   const [open, setOpen] = useCreateWorkspaceModal();
-  const {data, isLoading} = useGetWorkspaces();
+  const { data, isLoading } = useGetWorkspaces();
 
   const workspaceId = useMemo(() => data?.[0]?._id, [data]);
 
   useEffect(() => {
-    if(isLoading) return;
-    if(workspaceId) {
+    if (isLoading) return;
+    if (workspaceId) {
       router.replace(`/workspace/${workspaceId}`)
-    } else if(!open){
+    } else if (!open) {
       setOpen(true);
     }
   }, [workspaceId, isLoading, open, setOpen, router]);
 
   return (
-    <div>
-      <UserButton />
+    <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
+      <Loader className="size-6 animate-spin text-muted-foreground" />
     </div>
   );
 }
